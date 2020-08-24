8d9c54e7317c93a531c536ae3bcf111a3dde3804
loxt <emannuelloxt@hotmail.com>
2020-08-23 21:41:56 -0400

    chore: WIP feedForward function

--- 01-neural-network-feedforward/index.js
+++ 01-neural-network-feedforward/index.js
@@ -5,3 +5,18 @@ function sum(arr = []) {
 function gradientDescent(n = 0) {
   return n - (1 - n);
 }
+
+function feedForward(inputs = [], target = 0, epochs = 1) {
+  if (target <= 0) target = 0.1;
+  else if (target > 1) target = 1;
+
+  let weights = [];
+
+  for (let i = 0; i < inputs.length; i++) {
+    weights.push(Math.random());
+  }
+
+  for (let i = 1; i <= epochs; i++) {
+    
+  }
+}
