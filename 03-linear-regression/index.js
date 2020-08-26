23f72a7f9b0639d606501c5a920c6bed90476779
loxt <emannuelloxt@hotmail.com>
2020-08-26 11:02:16 -0400

    feat: add execution to linear regression

--- 03-linear-regression/index.js
+++ 03-linear-regression/index.js
@@ -17,7 +17,7 @@ function square(x = []) {
 }
 
 function summation(x = []) {
-  let temp = [];
+  let temp = 0;
 
   for (let i = 0; i < x.length; i++)
     temp += parseFloat(x[i]);
@@ -29,12 +29,37 @@ function average(x = []) {
   return summation(x) / x.length;
 }
 
-function linearRegression(x = [], y = [], p) {
+function results(x = [], y = [], p = 0) {
   const result1 = (summation(x) * summation(y)) / x.length;
   const result2 = (summation(x) * summation(x)) / x.length;
-  const result3 = summation(product(x, y)) - resultado1;
+  const result3 = summation(product(x, y)) - result1;
   const result4 = result3 / (summation(square(x)) - result2);
   const result5 = average(y) - (result4 * average(x)); 
 
   return ((result4 * p) + result5).toFixed(0);
 }
+
+function linearRegression(axisX = [], axisY = []) {
+  const sizeX = axisX.length;
+  const sizeY = axisY.length;
+
+  const tempX = axisX.slice(0, sizeY);
+  const tempY = axisY;
+
+  const diff = sizeX - sizeY;
+  
+  if (diff > 0) {
+    let regressions = [];
+
+    for (let i = 0; i < diff; i++) {
+      const temp = results(tempX, tempY, axisX[sizeY + i]);
+      regressions.push(temp);
+    }
+
+    const newY = tempY.concat(regressions);
+
+    console.log(`Axis X: ${axisX}\nAxis Y: ${newY}`);
+  }
+}
+
+linearRegression([1, 2, 3, 4, 5], [11, 22, 33, 44]);
