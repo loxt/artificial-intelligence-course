5b0dac00dfb117259a7dda604a23c302af82cf2c
loxt <emannuelloxt@hotmail.com>
2020-09-15 17:03:15 -0400

    chore: WIP train Bayes' theorem

--- 05-naive-bayes-probabilistic/index.js
+++ 05-naive-bayes-probabilistic/index.js
@@ -65,5 +65,80 @@ function frequency() {
 
 function classesQuantity() {
   const categories = frequency();
-  return +Object.keys(categories[0]).length - 1;
+  return parseInt(String(Object.keys(categories[0]).length - 1));
+}
+
+function sumClasses(arr = []) {
+  let sum = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    sum += parseInt(arr[i]);
+  }
+
+  return sum;
+}
+
+function totalPerClass() {
+  let totalClasses = [];
+  const nameClasses = returnClasses();
+  const strClasses = JSON.stringify(classes);
+
+  for (let i = 0; i < nameClasses.length; i++) {
+    totalClasses[nameClasses[i]] = countText(strClasses, nameClasses[i]);
+  }
+  return totalClasses;
+}
+
+function sumTotalClasses() {
+  const vetTemp = Object.values(totalPerClass());
+  let sum = 0;
+  for (let i = 0; i < vetTemp.length; i++) {
+    sum += parseFloat(String(vetTemp[i]));
+  }
+
+  return sum;
+}
+
+function occurrenceClassForInput(_input = '', _class = '') {
+  const categories = frequency();
+  let ret = 0;
+
+  categories.forEach((item) => {
+    if (item['Input'] === _input) {
+      ret = parseFloat(item[_class]);
+    }
+  });
+  return ret;
+}
+
+function naiveBayes(_input = '') {
+  const classNames = returnClasses();
+  const totalClass = totalPerClass();
+
+  const categories = frequency();
+  let sum = 0;
+  categories.forEach((item) => {
+    if (item['Input'] === _input) {
+      for (let i = 0; i < classNames.length; i++) {
+        sum += parseFloat(item[classNames[i]]);
+      }
+    }
+  });
+
+  let probability = [];
+
+  for (let i = 0; i < classNames.length; i++) {
+    probability[classNames[i]] =
+      (occurrenceClassForInput(_input, classNames[i]) / totalClass[classNames[i]]) *
+      (totalClass[classNames[i]] / sumTotalClasses()) /
+      (sum / sumTotalClasses());
+  }
+
+  return probability;
+}
+
+function train(_inputs = [], _classes = []) {
+  inputs = _inputs;
+  classes = _classes;
+
 }
